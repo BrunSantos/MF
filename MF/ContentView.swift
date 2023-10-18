f36d9e010a9a7e3f71852b80b30b6e248851731a
Bruno Santos <51135011+BrunSantos@users.noreply.github.com>
2023-10-18 21:47:25 +0100

    Initial Commit

---
+++ MF/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MF
+//
+//  Created by Bruno Santos on 18/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MF/MFApp.swift
@@ -0,0 +1,17 @@
+//
+//  MFApp.swift
+//  MF
+//
+//  Created by Bruno Santos on 18/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
